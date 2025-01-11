081a0ed02048c6badfac5ad162faf93ce5d3586c
kimzz14 <kimzz1004@gmail.com>
2025-01-11 11:45:18 +0900

    bug

--- sampleN09.aboveQ20/pipe/run01.sh
+++ sampleN09.aboveQ20/pipe/run01.sh
@@ -16,6 +16,6 @@ fi
 sh ./pipe/pairtools-merge.sh ${threadN} ${readID}
 sh ./pipe/pairtools-sort.sh  ${threadN} ${readID}
 sh ./pipe/pairtools-dedup.sh ${threadN} ${readID}.sorted
-sh ./pipe/library_QC.sh      ${threadN} ${readID}.sorted.dedup
+sh ./pipe/library_QC.sh                 ${readID}.sorted.dedup
 sh ./pipe/pairtools-split.sh ${threadN} ${readID}.sorted.dedup
 sh ./pipe/samtools-sort.sh   ${threadN} ${readID}.sorted.dedup.split
